b557524d44caaaa9258f1f438d7384e9a31b79f8
DiZay <74287343+ZDD-sup@users.noreply.github.com>
2023-04-28 01:42:05 +0600

    6.1

---
+++ controller/TodoController.hpp
@@ -0,0 +1,10 @@
+#include "oatpp/web/server/api/ApiController.hpp"
+#include "oatpp/core/macro/codegen.hpp"
+
+#include OATPP_CODEGEN_BEGIN(ApiController)
+// этот контроллер для работы с заметками (Todo)
+class MathController :public oatpp::web::server::api::ApiController {
+
+};
+
+#include OATPP_CODEGEN_END(ApiController)
